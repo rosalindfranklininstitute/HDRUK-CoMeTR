b6a3d64f4c4896676b72202590f2f68d2b53ce9f
yff85561 <netochukwu.onyiaji@rfi.ac.uk>
2023-07-13 11:22:09 +0000

    Mean Squared Error Comparison

--- src/global_metrics/MSE.py
+++ src/global_metrics/MSE.py
@@ -1,17 +1,41 @@
 import os
-import sys
 import argparse
-
 import h5py
 import numpy as np
 
+
 from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
 
 #function that reads two files and returns their mean square error
-def read_file(file1:str=None, file2:str=None, output_text:str=None):
-    if file1 is None or file2 is  None:
-        raise ValueError("Both file paths must be provided.")
-    else:
+def calc_mse(file1:str ='', file2:str ='', output_text:str =''):
+    """
+    Reads two files, calculates their mean squared error (MSE), and optionally saves the result to a text file.
+
+    Args:
+        file1 (str): Path to the first file. Required command-line argument: -f1/--file1.
+        file2 (str): Path to the second file. Required command-line argument: -f2/--file2.
+        output_text (str, optional): Path to the output text file. Optional command-line argument: -f3/--output_text.
+
+    Returns:
+        str: The path to the output text file if `output_text` is provided, otherwise an empty string.
+
+    Raises:
+        FileNotFoundError: If either `file1` or `file2` is not found.
+        h5py.FileError: If there is an error reading the HDF5 file.
+        Exception: If an unexpected error occurs.
+
+    Reads the specified files using the h5py library and accesses the voxel data within them.
+    Converts the data from 3D to 2D numpy arrays to calculate the mean squared error (MSE) between the arrays.
+    Prints the calculated MSE to the console.
+
+    If `output_text` is provided, saves the MSE value to the specified text file using numpy.savetxt.
+
+    Example usage:
+        $ python script.py -f1 path/to/file1.h5 -f2 path/to/file2.h5 -f3 path/to/output.txt
+
+    """
+    try:
         #read the file
         read_file1 = h5py.File(file1, 'r')
         read_file2 = h5py.File(file2, 'r')
@@ -20,30 +44,44 @@ def read_file(file1:str=None, file2:str=None, output_text:str=None):
         file_1_data = read_file1['entry']['data']['data']
         file_2_data = read_file2['entry']['data']['data']
 
+        # Ensure the arrays have the same shape
+        assert file_1_data.shape == file_2_data.shape
+
         #display shape of the file data for both files
         file1_name = os.path.basename(file1)
         file2_name = os.path.basename(file2)
         print(f'The shape of the {file1_name} file is {file_1_data.shape}')
         print(f'The shape of the {file2_name} file is {file_2_data.shape}')
 
-        # convert data from 3D  to 2D to use MSE metric
+        # convert data from 3D  to 2D numpy arrays to use MSE metric
         file1_2D = np.reshape(file_1_data, (file_1_data.shape[0], -1))
         file2_2D = np.reshape(file_2_data, (file_2_data.shape[0], -1))
 
         #calculate the mean square error
         mse = mean_squared_error(file1_2D, file2_2D)
-        print(f"The Mean Squared Error between the {file1_name} and {file2_name} is:", mse)
+        print(f"The Mean Squared Error between the {file1_name} and {file2_name} is:\n", mse)
 
-        if output_text is not None:
-            with open(output_text, 'a') as w:
-                w.write(str(f'{mse} is the mean squared error of {file1_name} and {file2_name}\n'))
-        return output_text
+        if output_text != '':
+            with open(output_text, 'a') as output_file:
+                np.savetxt(output_file, [mse], fmt='%s', delimiter='', newline='')
+
+        return mse
+
+    except FileNotFoundError as e:
+        print(f'File not found {str(e)}')
+
+    except OSError as e:
+        print(f'Error reading file {str(e)}')
+
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f1", '--file1', required=False)
-    parser.add_argument("-f2", '--file2', required=False)
+    parser = argparse.ArgumentParser(description='Calculate the MSE of two numpy arrays')
+    parser.add_argument("-f1", '--file1', required=True, help='Path to first file')
+    parser.add_argument("-f2", '--file2', required=True, help='Path to second file')
     parser.add_argument("-f3", '--output_text', required=False)
     args = parser.parse_args()
-    call_func = read_file(args.file1, args.file2, args.output_text)
+    call_func = calc_mse(args.file1, args.file2, args.output_text)
+
 
