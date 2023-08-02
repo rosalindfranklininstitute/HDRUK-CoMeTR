6bb7e49779407907ae60777b2a22e7ea8afc477e
yff85561 <netochukwu.onyiaji@rfi.ac.uk>
2023-08-02 12:11:16 +0000

    Update to MSE file

--- cometr/global_metrics/MSE.py
+++ cometr/global_metrics/MSE.py
@@ -42,7 +42,6 @@ class MSE:
             output_text (str, optional): Path to the file to store the result. Defaults to 'output.txt'.
 
         """
-        # super().__init__(file1, file2, file1_key, file2_key, output_text)
 
         self.file1 = file1
         self.file1_key = file1_key
@@ -56,16 +55,16 @@ class MSE:
         self.read_file1 = h5py.File(self.file1, "r")
         self.read_file2 = h5py.File(self.file2, "r")
 
-    @beartype()
-    def metric_calc(self):
+    @beartype
+    def metric_calc(self) -> float:
         """Calculates the mean squared error of the two numpy arrays and saves the result.
 
         This method reads data from two specified files.The data is then converted into PyTorch tensors.
         If there exists a CUDA-enabled GPU, the computation takes place on the GPU for enhanced performance.
-        
+
         Returns:
             float: The mean squared error of the two numpy arrays.
-        
+
         """
         file_1_data = self.read_file1[self.file1_key][:]
         file_2_data = self.read_file2[self.file2_key][:]
